74c51db752b5dbf01d433e9a1c2ea1a04ba15902
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-17 09:48:18 -0600

    updated success page

--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useMutation } from 'react';
 import Jumbotron from '../components/Jumbotron';
 import {ADD_ORDER} from '../utils/mutations';
-import {idbPromise} from '../../utils/helpers';
+import {idbPromise} from '../utils/helpers';
 
 function Success() {
 
